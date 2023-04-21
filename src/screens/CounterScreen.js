662a3dfa4eb08a05500e86111a5b1d9f044f556a
Daniel Li <zaoster777@gmail.com>
2023-04-21 15:59:05 -0500

    refactored counterScreen to use reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,20 +1,37 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, Image, Button, } from 'react-native';
+import React, { useReducer } from "react";
+import { View, Text, StyleSheet, Image, Button } from "react-native";
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({});
 
-})
+const reducer = (state, action) => {
+  if (action.type === "change_count") {
+    return { ...state, count: state.count + action.payload };
+  } else {
+    return state;
+  }
+};
 
 const CounterScreen = (props) => {
-    const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const { count } = state;
 
-    return (
-        <View>
-            <Button title='increase' onPress={() => {setCount(count + 1)}}/>
-            <Button title='decrease' onPress={() => {setCount(count - 1)}}/>
-            <Text>Current Count: {count}</Text>
-        </View>
-    );
+  return (
+    <View>
+      <Button
+        title="increase"
+        onPress={() => {
+          dispatch({ type: "change_count", payload: 1 });
+        }}
+      />
+      <Button
+        title="decrease"
+        onPress={() => {
+          dispatch({ type: "change_count", payload: -1 });
+        }}
+      />
+      <Text>Current Count: {count}</Text>
+    </View>
+  );
 };
 
 export default CounterScreen;
